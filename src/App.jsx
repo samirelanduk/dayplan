c5221a541ec3bd6f6a193f244c3da99eb2a74525
Sam Ireland <mail@samireland.com>
2025-04-14 22:40:28 +0100

    Start/end period

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useLocalStorageState } from "./hooks";
+
 const App = () => {
 
   const current = new Date();
   const [now, setNow] = useLocalStorageState("now", current.toISOString());
-  const [end, setEnd] = useLocalStorageState("end", new Date(current.getFullYear(), current.getMonth(), current.getDate(), 22, 0, 0, 0).toISOString());
+
+  const [period, setPeriod] = useLocalStorageState("period", [
+    new Date(current.getFullYear(), current.getMonth(), current.getDate() + (current.getHours() > 21 ? 1 : 0), 10, 0, 0, 0).toISOString(),
+    new Date(current.getFullYear(), current.getMonth(), current.getDate() + (current.getHours() > 21 ? 1 : 0), 22, 0, 0, 0).toISOString(),
+  ]);
+
   const [remainingWork, setRemainingWork] = useLocalStorageState("remainingWork", 0);
 
   const nowDt = new Date(now);
-  const endDt = new Date(end);
+  const periodDt = period.map(dt => new Date(dt));
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -29,14 +35,18 @@ const App = () => {
 
   const reset = () => {
     setNow(current.toISOString());
-    setEnd(new Date(current.getFullYear(), current.getMonth(), current.getDate(), 22, 0, 0, 0).toISOString());
+    setPeriod([
+      new Date(current.getFullYear(), current.getMonth(), current.getDate() + (current.getHours() > 21 ? 1 : 0), 10, 0, 0, 0).toISOString(),
+      new Date(current.getFullYear(), current.getMonth(), current.getDate() + (current.getHours() > 21 ? 1 : 0), 22, 0, 0, 0).toISOString(),
+    ]);
     setRemainingWork(0);
   }
 
-  const endString = dtToString(endDt);
-  const secondsRemaining = Math.round((endDt.getTime() - nowDt.getTime()) / 1000);
+  const startToUse = periodDt[0] > nowDt ? periodDt[0] : nowDt;
+  const secondsRemaining = Math.round((periodDt[1].getTime() - startToUse.getTime()) / 1000);
   const hoursRemaining = Math.floor(secondsRemaining / 3600);
   const minutesRemaining = Math.floor((secondsRemaining % 3600) / 60);
+
   const workPerHour = remainingWork / (secondsRemaining / 3600);
 
   return (
@@ -47,8 +57,39 @@ const App = () => {
         <button onClick={reset}>Reset</button>
       </div>
 
+      <div className="flex mt-4">
+        <div className="flex flex-col gap-1 border p-2 w-fit">
+          <label>Start</label>
+          <input type="datetime-local" value={dtToString(periodDt[0])} onChange={(e) => setPeriod([e.target.value, period[1]])} />
+        </div>
+        <div className="flex flex-col gap-1 border border-l-0 p-2 w-fit">
+          <label>End</label>
+          <input type="datetime-local" value={dtToString(periodDt[1])} onChange={(e) => setPeriod([period[0], e.target.value])} />
+        </div>
+      </div>
 
-      <div className="flex flex-col mt-8">
+      <div className="mt-4">
+        {hoursRemaining} hour{hoursRemaining === 1 ?  "" : "s"}
+        {minutesRemaining > 0 && ` ${minutesRemaining} minute${minutesRemaining === 1 ? "" : "s"}`}
+        {" "}remaining
+      </div>
+
+      <div className="mt-8">
+        <input
+          type="number"
+          className="w-12 text-right"
+          value={remainingWork}
+          onChange={(e) => setRemainingWork(e.target.value)}
+        />
+        <label>minutes of work remaining</label>
+      </div>
+
+      <div className="mt-8">
+        Need to do <span className="font-bold">{Math.ceil(workPerHour)}</span> minutes of work per hour
+      </div>
+
+
+      {/* <div className="flex flex-col mt-8">
         <label className="text-xs">End of day</label>
         <input
           type="datetime-local"
@@ -58,9 +99,9 @@ const App = () => {
         <div className="text-sm">
           {hoursRemaining} hour{hoursRemaining === 1 ? "" : "s"} {minutesRemaining} minute{minutesRemaining === 1 ? "" : "s"}
         </div>
-      </div>
+      </div> */}
 
-      <div className="mt-8">
+      {/* <div className="mt-8">
         <input
           type="number"
           className="w-12 text-right"
@@ -71,7 +112,7 @@ const App = () => {
         <div>
           {workPerHour}
         </div>
-      </div>
+      </div> */}
       
 
     </div>
