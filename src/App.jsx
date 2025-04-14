a5ba535b86d23e5712b72c005b4a57e7a30a6ba1
Sam Ireland <mail@samireland.com>
2025-04-14 16:24:20 +0100

    Display end date

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const todayAtTen = `${new Date().toISOString().slice(0, 10)}T22:00`;
+  
+  const [target, setTarget] = useState(todayAtTen);
+
   return (
-    <div>
-      Dayplan
+    <div className="flex flex-col gap-1 border p-2 w-fit">
+      <label>End of day</label>
+      <input
+        type="datetime-local"
+        value={target}
+        onChange={(e) => setTarget(e.target.value)}
+      />
     </div>
   );
 };
