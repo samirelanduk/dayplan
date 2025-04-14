2f982b874dcdcbac7860c838d727ad9ee4b760b5
Sam Ireland <mail@samireland.com>
2025-04-14 22:03:24 +0100

    Display time remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
 
-  const todayAtTen = `${new Date().toISOString().slice(0, 10)}T22:00`;
   
-  const [target, setTarget] = useState(todayAtTen);
-  const [remainingMinutes, setRemainingMinutes] = useState(0);
+  const [now, setNow] = useState(new Date());
+  const [end, setEnd] = useState(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 22, 0, 0, 0));
+  const [remainingWork, setRemainingWork] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const dtToString = dt => {
+    const year = dt.getFullYear();
+    const month = (dt.getMonth() + 1).toString().padStart(2, "0");
+    const day = dt.getDate().toString().padStart(2, "0");
+    const hour = dt.getHours().toString().padStart(2, "0");
+    const minute = dt.getMinutes().toString().padStart(2, "0");
+    const second = dt.getSeconds().toString().padStart(2, "0");
+    return `${year}-${month}-${day}T${hour}:${minute}:${second}`;
+  }
+
+  const endString = dtToString(end);
+  const secondsRemaining = Math.round((end.getTime() - now.getTime()) / 1000);
+  const hoursRemaining = Math.floor(secondsRemaining / 3600);
+  const minutesRemaining = Math.floor((secondsRemaining % 3600) / 60);
+  const workPerHour = remainingWork / (secondsRemaining / 3600);
 
   return (
+    <div className="p-2">
+      <div>{now.toLocaleString()}</div>
+
+
+      <div className="flex flex-col mt-8">
+        <label className="text-xs">End of day</label>
+        <input
+          type="datetime-local"
+          value={endString}
+          onChange={(e) => setEnd(new Date(e.target.value))}
+        />
+        <div className="text-sm">
+          {hoursRemaining} hour{hoursRemaining === 1 ? "" : "s"} {minutesRemaining} minute{minutesRemaining === 1 ? "" : "s"}
+        </div>
+      </div>
+
+      <div className="mt-8">
+        <input
+          type="number"
+          className="w-12 text-right"
+          value={remainingWork}
+          onChange={(e) => setRemainingWork(e.target.value)}
+        />
+        <label>Minutes remaining</label>
+        <div>
+          {workPerHour}
+        </div>
+      </div>
+      
+
+    </div>
+  )
+}
+
+  
+  /* const [remainingMinutes, setRemainingMinutes] = useState(0);
+
+  const todayAtTen = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 22, 0, 0, 0);
+
+  const secondsRemaining = Math.round((todayAtTen.getTime() - now.getTime()) / 1000);
+
+   */
+
+  /* return (
     <div>
       <div className="flex flex-col gap-1 border p-2 w-fit">
         <label>End of day</label>
@@ -16,6 +83,8 @@ const App = () => {
           value={target}
           onChange={(e) => setTarget(e.target.value)}
         />
+        <label>Remaining minutes</label>
+        <div>{secondsRemaining}</div>
       </div>
       <div className="flex flex-col gap-1 border p-2 w-fit">
         <label>Remaining minutes</label>
@@ -26,8 +95,7 @@ const App = () => {
         />
       </div>
     </div>
-  );
-};
+  ); */
 
 App.propTypes = {
   
