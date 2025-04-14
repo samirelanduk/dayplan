b2cd5b0b25fbe7f2f35274cdb8dc086155605262
Sam Ireland <mail@samireland.com>
2025-04-14 16:26:16 +0100

    Remaining minutes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ const App = () => {
   const todayAtTen = `${new Date().toISOString().slice(0, 10)}T22:00`;
   
   const [target, setTarget] = useState(todayAtTen);
+  const [remainingMinutes, setRemainingMinutes] = useState(0);
 
   return (
-    <div className="flex flex-col gap-1 border p-2 w-fit">
-      <label>End of day</label>
-      <input
-        type="datetime-local"
-        value={target}
-        onChange={(e) => setTarget(e.target.value)}
-      />
+    <div>
+      <div className="flex flex-col gap-1 border p-2 w-fit">
+        <label>End of day</label>
+        <input
+          type="datetime-local"
+          value={target}
+          onChange={(e) => setTarget(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col gap-1 border p-2 w-fit">
+        <label>Remaining minutes</label>
+        <input
+          type="number"
+          value={remainingMinutes}
+          onChange={(e) => setRemainingMinutes(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
